a38370cd0b2edcf6f048a4be2f7513065ffdbfe5
Lucas de Sena <36707595+lucasbnu@users.noreply.github.com>
2023-02-01 15:26:28 -0300

    Aula tarde

---
+++ ExemploConstrutor/src/main/java/com/mycompany/exemploconstrutor/ExemploConstrutor.java
@@ -0,0 +1,10 @@
+package com.mycompany.exemploconstrutor;
+
+public class ExemploConstrutor {
+
+    public static void main(String[] args) {
+        carro objetoCarro = new carro("Jac");
+        System.out.println(objetoCarro.getMontadora());
+        
+    }
+}
---
+++ exemploContrutor/src/main/java/com/mycompany/exemplocontrutor/ExemploContrutor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplocontrutor;
+
+/**
+ *
+ * @author mac
+ */
+public class ExemploContrutor {
+
+    public static void main(String[] args) {
+        //Pessoa objetoPessoa = new Pessoa("lucas",23);
+        Pessoa objPessoa2 = new Pessoa();
+        //objetoPessoa.setIdade(18);
+        //objetoPessoa.setNome("Joao");
+    }
+}
